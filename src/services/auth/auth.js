fce114c0714b8c25a12e9efc31fe0dcf5755eb52
jmlinger <jmlinger10@gmail.com>
2022-12-01 16:27:30 -0300

    feat: it puts jwt's expires time only for customers.

--- src/services/auth/auth.js
+++ src/services/auth/auth.js
@@ -3,14 +3,13 @@ const jwt = require('jsonwebtoken');
 
 const { JWT_SECRET } = process.env;
 
-const JWT_CONFIG = {
-  expiresIn: '120m',
-  algorithm: 'HS256',
-};
-
 const genToken = (data) => {
-  const token = jwt.sign({ data }, JWT_SECRET, JWT_CONFIG);
+  const JWT_CONFIG = data.role === 'customer'
+  ? { expiresIn: '120m', algorithm: 'HS256' }
+  : { algorithm: 'HS256' };
 
+  const token = jwt.sign({ data }, JWT_SECRET, JWT_CONFIG);
+  
   return token;
 };
 
